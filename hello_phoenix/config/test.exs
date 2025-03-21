19fad00455e19762fa7fa6312ae18224180a08b6
Awesome <awesome_devnet@outlook.com>
2025-03-22 01:52:48 +0900

    update

---
+++ hello_phoenix/config/test.exs
@@ -0,0 +1,34 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :hello_phoenix, HelloPhoenix.Repo,
+  database: "hello_phoenix_test.db#{System.get_env("MIX_TEST_PARTITION")}",
+  pool_size: 5,
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :hello_phoenix, HelloPhoenixWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "TQ7EPi9a8ZjdEeW69UkFyU3HbU9NVbJuWIbv9DpRpJ3r01pxDkGJfvBnQMehOvya",
+  server: false
+
+# In test we don't send emails
+config :hello_phoenix, HelloPhoenix.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
+
+# Enable helpful, but potentially expensive runtime checks
+config :phoenix_live_view,
+  enable_expensive_runtime_checks: true
---
+++ hello_phoenix/lib/hello_phoenix_web/controllers/task_html.ex
@@ -0,0 +1,13 @@
+defmodule HelloPhoenixWeb.TaskHTML do
+  use HelloPhoenixWeb, :html
+
+  embed_templates "task_html/*"
+
+  @doc """
+  Renders a task form.
+  """
+  attr :changeset, Ecto.Changeset, required: true
+  attr :action, :string, required: true
+
+  def task_form(assigns)
+end
